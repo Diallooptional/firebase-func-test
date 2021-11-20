da29739613b27b47f72fd423abab9e98e889d3c8
Optional <diallooptional@gmail.com>
2021-11-20 16:00:39 +0200

    upodated comments

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 /**
  * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 'use strict';
 
@@ -22,25 +10,13 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
+//Geofire SDK 
+const geofire = require('geofire');
+
+
 admin.initializeApp();
 // [END import]
-
-// [START addMessage]
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-// [START addMessageTrigger]
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-// [END addMessageTrigger]
-  // Grab the text parameter.
-  const original = req.query.text;
-  // [START adminSdkPush]
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/messages').push({original: original});
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
-  // [END adminSdkPush]
-});
-// [END addMessage]
 
 // [START makeUppercase]
 // Listens for new messages added to /messages/:pushId/original and creates an
@@ -77,6 +53,8 @@ exports.deliveryRequest = functions.firestore
 
         //TODO: GeoFire
         //send data to all in keyEntered
+
+        //Store a Geo query readab;le by client, like in : https://firebase.google.com/docs/firestore/solutions/geoqueries#java
       });
 
 // [END deliveryRequest]
